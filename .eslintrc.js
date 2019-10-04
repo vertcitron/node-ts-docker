db2ee10ba550f87373dc150ed1440ce312962c87
Stéphane Souron <stef@vertcitron.fr>
2019-10-04 22:38:39 +0200

    added prettier with auto format on save.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
   plugins: [
     '@typescript-eslint',
     'jest'
   ],
-  extends: [
-    'plugin:@typescript-eslint/recommended'
+  overrides: [
+    { files: ['**/*.test.js','**/*.test.ts'], env: { 'jest': true } }
   ],
-  "overrides": [
-    {
-      "files": [
-        "**/*.test.js",
-        "**/*.test.ts"
-      ],
-      "env": {
-        "jest": true
-      }
-    }
-  ]
+  rules: {
+    semi: ['error', 'never']
+  }
 }
