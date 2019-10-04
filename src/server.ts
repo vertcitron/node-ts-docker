4314c6c25252a23e2f77a15c5bf04ad2910091db
Stéphane Souron <stef@vertcitron.fr>
2019-10-04 23:41:06 +0200

    added traefik to docker-compose

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import express, { Application, Request, Response } from 'express'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require('../package.json')
 
 export const server: Application = express()
 
 server.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Express server running in its container.' })
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+      <style>
+        html, body { width: 100%; height: 100%; margin: 0; font-family: sans-serif; }
+        body { display: flex; flex-flow: column nowrap; justify-content: center; align-items: center; }
+      </style>
+    </head>
+    <body>
+      <h2>Node-TS-Docker Server</h2>
+      <p>Version ${pkg.version}</p>
+    </body>
+  </html>
+  `)
 })
